b6334421fc8fac060e5b789e094a49210acfe16c
Unknown <fabienbouteyre@hotmail.fr>
2018-11-30 12:00:31 +0100

    Add plan

--- temperatureApp/src/app/app.module.ts
+++ temperatureApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DxVectorMapModule } from 'devextreme-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,19 @@ import { Floor1Component } from './floor1/floor1.component';
 import { Floor2Component } from './floor2/floor2.component';
 import { TemperatureDetailComponent } from './temperature-detail/temperature-detail.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PlanComponent } from './plan/plan.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Floor1Component,
     Floor2Component,
-    TemperatureDetailComponent
+    TemperatureDetailComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
+    DxVectorMapModule,
     HttpClientModule,
     AppRoutingModule,
 
